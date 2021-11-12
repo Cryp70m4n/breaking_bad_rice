8f5d27fd7f4f78a5337ceb5b972bcf7d2f9a0636
Cryp70m4n <70487877+Cryp70m4n@users.noreply.github.com>
2021-11-12 19:29:50 +0000

    Create config.h

---
+++ dmenu/config.h
@@ -0,0 +1,23 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"monospace:size=10"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#a8a98b", "#342b21" },
+	[SchemeSel] = { "#989970", "#c4b49e" },
+	[SchemeOut] = { "#000000", "#b2b3ac" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
